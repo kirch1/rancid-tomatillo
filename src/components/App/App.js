87d014521decef3ed046f9b1798e10a97b2b12d9
Shane Misra <shane.misra@gmail.com>
2023-03-25 11:49:25 -0600

    enable first fetch call - all movies data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,26 @@ import './App.css';
 import React, {Component} from 'react';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import FilterForm from '../FilterForm/FilterForm'
-import movieData from '../testData';
+// import movieData from '../testData';
 import MovieDetails from '../MovieDetails/MovieDetails';
 import logo from '../../assets/avocado.svg';
+import {fetchAllMovies, fetchMovieDetails} from '../../apiCalls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      allMovies: movieData,
+      allMovies: [],
       selectedMovie: 0
     };
+  }
+  componentDidMount() {
+    fetchAllMovies()
+      .then(data => {
+      this.setState({allMovies: data.movies})
+      console.log(this.state.allMovies)
+      })
+      .catch(error => console.log(error))
   }
 
   selectMovie = id => {
@@ -26,7 +35,7 @@ class App extends Component {
           <img src={logo} className='headerLogo'/>
           <h1>expired avocados</h1>
         </header>
-        {this.state.selectedMovie ? <MovieDetails selectMovie={this.selectMovie}/> : <MoviesContainer movies={this.state.allMovies.movies} selectMovie={this.selectMovie}/> }
+        {this.state.selectedMovie ? <MovieDetails selectMovie={this.selectMovie}/> : <MoviesContainer movies={this.state.allMovies} selectMovie={this.selectMovie}/> }
         {!this.state.selectedMovie && <FilterForm />}
       </main>
     );
